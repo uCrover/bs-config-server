dbeedb7555adbb87cf4aee056acf1fc90edebaca
uCrover <chris66xd@gmail.com>
2022-03-03 12:03:31 -0500

    add config finales

--- src/main/java/com/nttdata/config/ConfigServiceApplication.java
+++ src/main/java/com/nttdata/config/ConfigServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-//aqui habilitamos el configserver
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
